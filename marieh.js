ce7d10bdf12aae62a11359d3dc5306bd22e42cfc
Maali <lwasalaw@gmail.com>
2020-04-09 15:25:24 +0300

    javascript file

---
+++ marieh.js
@@ -0,0 +1,15 @@
+const grade = 80;
+if(grade>80){
+
+    console.log("you have an A")
+
+}else if
+    (grade>=65 && grade <80){
+    console.log("you have a B")
+}else if (grade >=50 && grade < 65){
+console.log("you passed")
+
+}else if 
+(grade < 50){
+    console.log ("you failed ")
+}
